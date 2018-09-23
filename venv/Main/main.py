841b52369242749296c2613d64dfdbcca07eb197
dikki456 <andropocket112gmail.com>
2018-09-23 09:13:24 +0530

    system file

---
+++ venv/Main/main.py
@@ -0,0 +1,19 @@
+import math
+
+def splitCheck(total,noOfPeople):
+
+    return  math.ceil(total // noOfPeople)
+
+try:
+    bill = float(input("Enter billing ammount: "))
+    pson = int(input("Enter no. of person: "))
+
+except ValueError:
+    print("Lol,that not a valid value ")
+else:
+    pPCounter = splitCheck(bill, pson)
+    print("Price per person {}$ ".format(pPCounter))
+
+
+
+
