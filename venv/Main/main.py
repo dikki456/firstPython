cda6e93c33e98835ad7a805f2146fc61ecce1075
dikki456 <andropocket112gmail.com>
2018-09-24 14:51:12 +0530

    added exception handling of false value

--- venv/Main/main.py
+++ venv/Main/main.py
@@ -2,17 +2,18 @@ import math
 
 def splitCheck(total,noOfPeople):
 
+    if noOfPeople <=1:
+        raise ValueError("More than one person is required")
     return  math.ceil(total // noOfPeople)
 
 try:
     bill = float(input("Enter billing ammount: "))
     pson = int(input("Enter no. of person: "))
-    sSon = input("Checking the amount of the bill")
+    pPCounter = splitCheck(bill, pson)
 
 except ValueError:
     print("Lol,that not a valid value ")
 else:
-    pPCounter = splitCheck(bill, pson)
     print("Price per person {}$ ".format(pPCounter))
     print("Hello every one how are you what are you doing  ".format(pPCounter))
 
