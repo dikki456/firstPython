e3dc4ef4f7eb592ed9439732a52bfec282fdfab2
dikki456 <andropocket112gmail.com>
2018-09-24 15:10:32 +0530

    Revert "added exception handling of false value"
    This reverts commit 9bb02f45336f43f5bc9ac202defaf9d69318aa72.

--- venv/Main/main.py
+++ venv/Main/main.py
@@ -1,6 +1,7 @@
 import math
 
 def splitCheck(total,noOfPeople):
+
     if noOfPeople <=1:
         raise ValueError("More than one person is required")
     return  math.ceil(total // noOfPeople)
@@ -10,9 +11,8 @@ try:
     pson = int(input("Enter no. of person: "))
     pPCounter = splitCheck(bill, pson)
 
-except ValueError as err:
+except ValueError:
     print("Lol,that not a valid value ")
-    print("({})".format(err))
 else:
     print("Price per person {}$ ".format(pPCounter))
     print("Hello every one how are you what are you doing  ".format(pPCounter))
